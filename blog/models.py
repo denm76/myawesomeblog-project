f6919c7bb275e4180fdcb5b558e4f0ab039c3562
denm76 <denm76@gmail.com>
2021-04-05 12:42:02 +0300

    Post migraton updated

--- blog/models.py
+++ blog/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Post(models.Model):
     post_text = models.TextField()
     post_title = models.CharField(max_length=100)
-    post_date = models.DateTimeField()
+    post_date = models.DateTimeField(auto_now_add = True)
     post_image = models.ImageField(upload_to='event_images/')
