faee72a055d44cb7463522fef0002c53ab302750
denm76 <denm76@gmail.com>
2021-04-05 12:10:24 +0300

    Make migration for Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    post_text = models.CharField(max_length=300)
+    post_title = models.CharField(max_length=100)
+    post_date = models.DateField(null=True, blank=True)
+    post_image = models.ImageField(upload_to='post_images/')
